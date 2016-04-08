f8296bce5efd71ff310757fe2efc71827a0f0023
Ian Paul Barrachina Vega <ian.barrachina.ext@telefonica.com>
2016-04-08 19:52:00 +0200

    Arreglados mensajes flash de Devise

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -2,6 +2,7 @@ class MainPagesController < ApplicationController
   def home
     if user_signed_in? 
       redirect_to current_user
+      flash[:success] = "Has sido redirigido a tu pagina principal"
     end
   end
 end
