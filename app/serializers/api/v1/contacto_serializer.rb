d4ea04c73819defd41f8ed192e427602b554a688
Ian Paul Barrachina Vega <ian.barrachina.ext@telefonica.com>
2016-04-11 21:26:34 +0200

    Disparo al aire

--- app/serializers/api/v1/contacto_serializer.rb
+++ app/serializers/api/v1/contacto_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::ContactoSerializer < Api::V1::BaseSerializer
-  attributes :id, :nombre, :apellidos, :email, :foto, :created_at, :updated_at
+  attributes :id, :nombre, :apellidos, :email, :foto, :tipo => @contacto.tipo.tipo, :created_at, :updated_at
 
   has_many :telefonos
 
